c8faf6c467482d1aa438b95c070ed687ac038971
brieuc.mandin <brieuc.mandin@etu.estia.fr>
2025-01-29 14:52:58 +0100

    Test Unitaire Validés

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.kt
@@ -3,18 +3,30 @@ package com.openclassrooms.magicgithub.repository
 import com.openclassrooms.magicgithub.api.ApiService
 import com.openclassrooms.magicgithub.model.User
 
+
 class UserRepository(
     private val apiService: ApiService
 ) {
+    // Liste locale pour stocker les utilisateurs (peut aussi être remplacée par une base de données ou autre mécanisme)
+    private val userList = mutableListOf<User>()
+
     fun getUsers(): List<User> {
-        TODO("Should return the list of users retrieved from the api service")
+
+        // Pour simuler l'appel à l'API et obtenir une liste d'utilisateurs
+        val usersFromApi = apiService.getUsers() // Appel à l'API pour obtenir les utilisateurs
+        userList.clear() // Vider la liste locale (si nécessaire)
+        userList.addAll(usersFromApi) // Ajouter les utilisateurs récupérés depuis l'API à la liste locale
+        return userList
     }
 
     fun addRandomUser() {
-        TODO("Should add a random user")
+        // Appeler la fonction addRandomUser() de FakeApiService pour ajouter un utilisateur aléatoire
+        apiService.addRandomUser()
     }
 
     fun deleteUser(user: User) {
-        TODO("Should remove the user")
+        apiService.deleteUser(user) // Retirer l'utilisateur de la liste
     }
+
+
 }
